5fb94379d6a99cb5597340b2ee7ff65de1bde04c
Omar <omartsadek@gmail.com>
2022-04-28 10:36:24 -0400

    commit tree 7548d53b2ae8405bbfeae21af970caee1e9020c2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+//      In index.js, build a function named writeCards() that accepts two arguments: an array of string names, and an event name. 
+//      Create a for loop with a counter that starts at 0 and increments at the end of each loop. 
+//      The for loop should stop once it has iterated over the length of the array.
+ function writeCards(names,message){
+        let cards = []
+            for(let i = 0; i < names.length; i++) {
+                cards.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+                //console.log(names[i])
+          }
+                return cards
+      }
+
+function countDown(num){
+ while(num >= 0){
+   console.log(num--)
+ }
+}
